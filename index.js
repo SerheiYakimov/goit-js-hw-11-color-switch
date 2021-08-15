b071e6cdbe5d5a22512721f8a2aeda585e99384e
SerheiYakimov <ysergey2901@gmail.com>
2021-08-15 12:36:26 +0300

    добавил функционал рандомного цвета

--- index.js
+++ index.js
@@ -7,26 +7,35 @@ const colors = [
   '#795548',
 ];
 
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-
 const refs = {
     start: document.querySelector('button[data-action=start]'),
     stop: document.querySelector('button[data-action=stop]'),
     body: document.querySelector('body')
 }
 
-
 refs.start.addEventListener('click', onStartColor);
 refs.stop.addEventListener('click', onStopColor);
 
 let timerId = null;
 
-
-
 function onStartColor(e) {
     
-    timerId = setInterval(colors, 1000);
+    timerId = setInterval( setBodyBgColor, 1000);
+    
 };
+
+
+function setBodyBgColor() {
+    const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+    const rundomIndex = randomIntegerFromInterval(0, colors.length);
+
+    refs.body.style.backgroundColor = colors[rundomIndex];
+    
+   
+}
+
+function onStopColor(e) {
+    clearInterval(timerId);
+}
